2b0b86cccc65a000352f0fe84624b52fd012144b
MateusLopesNunes <mateuslopesnunes@outlook.com>
2022-07-02 10:31:20 -0300

    editar contatos funcionando

--- listaCidades.php
+++ listaCidades.php
@@ -1,22 +1,42 @@
 <?php
 
-include "conectaBanco.php";
+    session_start();
 
-$codigoEstado = $_GET['codigoEstado'];
+    $verificaUsuarioLogado = $_SESSION['verificaUsuarioLogado'];
 
-$sqlCidades = "SELECT codigoCidade, nomeCidade FROM cidades WHERE codigoEstado=:codigoEstado";
+    if ($verificaUsuarioLogado){
+        include "conectaBanco.php";
 
+        if (isset($_GET['codigoEstado'])) {
+            $codigoEstado = $_GET['codigoEstado'];
+        }else {
+            $codigoCidade = '';
+        }
 
-$sqlCidadesST = $conexao->prepare($sqlCidades);
-$sqlCidadesST->bindValue(':codigoEstado', $codigoEstado);
+        if(isset($_GET['codigoCidade'])){
+            $codigoCidade = $_GET['codigoCidade'];
+        }else{
+            $codigoCidade = '';
+        }
 
-$sqlCidadesST->execute();
-$resultadoCidades = $sqlCidadesST->fetchALL();
+        $sqlCidades = "SELECT codigoCidade, nomeCidade FROM cidades WHERE codigoEstado=:codigoEstado";
 
-echo "<option value=\"\">Escolha a cidade</option>\n";
+        $sqlCidadesST = $conexao->prepare($sqlCidades);
+        $sqlCidadesST->bindValue(':codigoEstado', $codigoEstado);
+        $sqlCidadesST->execute();
+        $resultadoCidades = $sqlCidadesST->fetchAll();
 
-foreach ($resultadoCidades as list($codigoCidade, $nomeCidade)){
-    echo "<option value=\"$codigoCidade\">$nomeCidade</option>";
-}
+        echo "<option value=\"\">Escolha sua cidade</option>\n";
+
+        foreach ($resultadoCidades as list($codCidade, $nomeCidade)) {
+            if(!empty($codigoCidade ) && $codigoCidade == $codCidade ){
+                $selected = 'selected';
+            }else {
+                $selected = '';
+            }
+            echo "<option value=\"$codCidade\" $selected>$nomeCidade</option>\n";
+        }
+
+    }
 
 ?>
